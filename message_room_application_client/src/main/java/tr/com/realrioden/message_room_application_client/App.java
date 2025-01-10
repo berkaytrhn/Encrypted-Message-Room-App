f55c971af6c75fbed78bede830997310b3668d7e
Berkay <berkaytrhn@gmail.com>
2025-01-10 22:58:09 +0300

    initial commit of 3 apps

---
+++ message_room_application_client/src/main/java/tr/com/realrioden/message_room_application_client/App.java
@@ -0,0 +1,13 @@
+package tr.com.realrioden.message_room_application_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
